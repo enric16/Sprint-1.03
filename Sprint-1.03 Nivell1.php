004dad21d9f2042ce74ff87a056ad20d69971232
enricSalvador <esalvador94@gmail.com>
2025-03-24 10:58:49 +0100

    upload local files

---
+++ Sprint-1.03 Nivell1.php
@@ -0,0 +1,43 @@
+<?php
+//EXERCICI 1
+echo "<h4>" . "Exercici 1" . "</h4>";
+$numeros = [1,3,5,7,9];
+
+echo $numeros [0] . "<br>";
+echo $numeros [1] . "<br>";
+echo $numeros [2] . "<br>";
+echo $numeros [3] . "<br>";
+echo $numeros [4] . "<br>";
+
+//EXERCICI 2
+echo "<h4>" . "Exercici 2" . "</h4>";
+
+echo count($numeros) . "<br>";
+unset($numeros[1]);
+$numeros = array_values($numeros);
+print_r($numeros);
+
+//EXERCICI 3
+echo "<h4>" . "Exercici 3" . "</h4>";
+
+$paraules = ["gat","gos","gorila","iguana","guacamai", "cangur","orangutan","pingui"];
+$caracter = "g";
+
+function conteCaracter ($paraules, $caracter) {
+    foreach ($paraules as $paraula) {
+        if (strpos($paraula, $caracter) === false) {
+        return "False";
+        }
+    }
+    return "True";
+}
+
+echo conteCaracter ($paraules,$caracter);
+
+//EXERCICI 4
+echo "<h4>" . "Exercici 4" . "</h4>";
+
+$arrayAssociatiu = array("nom" => "Enric", "edat" => "31", "email" => "esalvador94@gmail.com", "menjar favorit" => "croquetes");
+print_r($arrayAssociatiu);
+
+?>
